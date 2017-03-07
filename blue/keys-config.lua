baeb2792edb3fa3e4964ac17c9a97ee05a5a22e5
worron <worrongm@gmail.com>
2017-03-08 00:04:27 +0300

    Blue config minor update

--- blue/keys-config.lua
+++ blue/keys-config.lua
@@ -1,1 +1,467 @@
-../colorless/keys-config.lua
+-----------------------------------------------------------------------------------------------------------------------
+--                                          Hotkeys and mouse buttons config                                         --
+-----------------------------------------------------------------------------------------------------------------------
+
+-- Grab environment
+local table = table
+local awful = require("awful")
+local beautiful = require("beautiful")
+
+local redflat = require("redflat")
+
+-- Initialize tables and vars for module
+-----------------------------------------------------------------------------------------------------------------------
+local hotkeys = { mouse = {}, raw = {}, keys = {}, fake = {} }
+
+-- key aliases
+local apprunner = redflat.float.apprunner
+local appswitcher = redflat.float.appswitcher
+local current = redflat.widget.tasklist.filter.currenttags
+local allscr = redflat.widget.tasklist.filter.allscreen
+local laybox = redflat.widget.layoutbox
+local redtip = redflat.float.hotkeys
+local laycom = redflat.layout.common
+local redtitle = redflat.titlebar
+
+-- Key support functions
+-----------------------------------------------------------------------------------------------------------------------
+local focus_switch_byd = function(dir)
+	return function()
+		awful.client.focus.bydirection(dir)
+		if client.focus then client.focus:raise() end
+	end
+end
+
+local function minimize_all()
+	for _, c in ipairs(client.get()) do
+		if current(c, mouse.screen) then c.minimized = true end
+	end
+end
+
+local function minimize_all_except_focused()
+	for _, c in ipairs(client.get()) do
+		if current(c, mouse.screen) and c ~= client.focus then c.minimized = true end
+	end
+end
+
+local function restore_all()
+	for _, c in ipairs(client.get()) do
+		if current(c, mouse.screen) and c.minimized then c.minimized = false end
+	end
+end
+
+local function kill_all()
+	for _, c in ipairs(client.get()) do
+		if current(c, mouse.screen) and not c.sticky then c:kill() end
+	end
+end
+
+local function focus_to_previous()
+	awful.client.focus.history.previous()
+	if client.focus then client.focus:raise() end
+end
+
+local function restore_client()
+	local c = awful.client.restore()
+	if c then client.focus = c; c:raise() end
+end
+
+local function toggle_placement(env)
+	env.set_slave = not env.set_slave
+	redflat.float.notify:show({
+		text = (env.set_slave and "Slave" or "Master") .. " placement",
+		icon = beautiful.icon and beautiful.icon.warning or nil
+	})
+end
+
+local function tag_numkey(i, mod, action)
+	return awful.key(
+		mod, "#" .. i + 9,
+		function ()
+			local screen = awful.screen.focused()
+			local tag = screen.tags[i]
+			if tag then action(tag) end
+		end
+	)
+end
+
+local function client_numkey(i, mod, action)
+	return awful.key(
+		mod, "#" .. i + 9,
+		function ()
+			if client.focus then
+				local tag = client.focus.screen.tags[i]
+				if tag then action(tag) end
+			end
+		end
+	)
+end
+
+
+-- Build hotkeys depended on config parameters
+-----------------------------------------------------------------------------------------------------------------------
+function hotkeys:init(args)
+
+	-- Init vars
+	------------------------------------------------------------
+	local args = args or {}
+	local env = args.env
+	local mainmenu = args.menu
+
+	self.mouse.root = (awful.util.table.join(
+		awful.button({ }, 3, function () mainmenu:toggle() end),
+		awful.button({ }, 4, awful.tag.viewnext),
+		awful.button({ }, 5, awful.tag.viewprev)
+	))
+
+	-- Keys for widgets, layouts and other secondary stuff
+	--------------------------------------------------------------------------------
+
+	-- Layouts
+	------------------------------------------------------------
+	local layout_tile = {
+		{
+			{ env.mod }, "l", function () awful.tag.incmwfact( 0.05) end,
+			{ description = "Increase master width factor", group = "Layout" }
+		},
+		{
+			{ env.mod }, "j", function () awful.tag.incmwfact(-0.05) end,
+			{ description = "Decrease master width factor", group = "Layout" }
+		},
+		{
+			{ env.mod }, "i", function () awful.client.incwfact( 0.05) end,
+			{ description = "Increase window factor of a client", group = "Layout" }
+		},
+		{
+			{ env.mod }, "k", function () awful.client.incwfact(-0.05) end,
+			{ description = "Decrease window factor of a client", group = "Layout" }
+		},
+		{
+			{ env.mod, }, "+", function () awful.tag.incnmaster( 1, nil, true) end,
+			{ description = "Increase the number of master clients", group = "Layout" }
+		},
+		{
+			{ env.mod }, "-", function () awful.tag.incnmaster(-1, nil, true) end,
+			{ description = "Decrease the number of master clients", group = "Layout" }
+		},
+		{
+			{ env.mod, "Control" }, "+", function () awful.tag.incncol( 1, nil, true) end,
+			{ description = "Increase the number of columns", group = "Layout" }
+		},
+		{
+			{ env.mod, "Control" }, "-", function () awful.tag.incncol(-1, nil, true) end,
+			{ description = "Decrease the number of columns", group = "Layout" }
+		},
+	}
+
+	laycom:set_keys("tile", layout_tile)
+
+	-- Apprunner widget
+	------------------------------------------------------------
+	local apprunner_keys = {
+		{
+			{ env.mod }, "k", function() apprunner:down() end,
+			{ description = "Select next item", group = "Navigation" }
+		},
+		{
+			{ env.mod }, "i", function() apprunner:up() end,
+			{ description = "Select previous item", group = "Navigation" }
+		},
+	}
+
+	apprunner:set_keys(awful.util.table.join(apprunner.keys, apprunner_keys))
+
+	-- Menu widget
+	------------------------------------------------------------
+	local menu_keys = {
+		{
+			{ env.mod }, "k", redflat.menu.action.down,
+			{ description = "Select next item", group = "Navigation" }
+		},
+		{
+			{ env.mod }, "i", redflat.menu.action.up,
+			{ description = "Select previous item", group = "Navigation" }
+		},
+		{
+			{ env.mod }, "j", redflat.menu.action.back,
+			{ description = "Go back", group = "Navigation" }
+		},
+		{
+			{ env.mod }, "l", redflat.menu.action.enter,
+			{ description = "Open submenu", group = "Navigation" }
+		},
+	}
+
+	redflat.menu:set_keys(awful.util.table.join(redflat.menu.keys, menu_keys))
+
+	-- Appswitcher
+	------------------------------------------------------------
+	appswitcher_keys = {
+		{
+			{ env.mod }, "a", function() appswitcher:switch() end,
+			{ description = "Select next app", group = "Navigation" }
+		},
+		{
+			{ env.mod }, "q", function() appswitcher:switch({ reverse = true }) end,
+			{ description = "Select previous app", group = "Navigation" }
+		},
+		{
+			{ env.mod }, "Super_L", function() appswitcher:hide() end,
+			{ description = "Exit", group = "Action" }
+		},
+		{
+			{}, "Escape", function() appswitcher:hide(true) end,
+			{ description = "Exit", group = "Action" }
+		},
+		{
+			{ env.mod }, "F1", function() redtip:show()  end,
+			{ description = "Show hotkeys helper", group = "Help" }
+		},
+	}
+
+	appswitcher:set_keys(appswitcher_keys)
+
+
+	-- Global keys
+	--------------------------------------------------------------------------------
+	self.raw.root = {
+		{
+			{ env.mod }, "F1", function() redtip:show() end,
+			{ description = "Show hotkeys helper", group = "Main" }
+		},
+		{
+			{ env.mod }, "F2", function () redflat.service.navigator:run() end,
+			{ description = "Window control mode", group = "Main" }
+		},
+		{
+			{ env.mod }, "F3", function () toggle_placement(env) end,
+			{ description = "Switch master/slave window placement", group = "Main" }
+		},
+		{
+			{ env.mod, "Control" }, "r", awesome.restart,
+			{ description = "Reload awesome", group = "Main" }
+		},
+
+		{
+			{ env.mod }, "Return", function() awful.spawn(env.terminal) end,
+			{ description = "Open a terminal", group = "Applications" }
+		},
+		{
+			{ env.mod, "Mod1" }, "space", function() awful.spawn("clipflap --show") end,
+			{ description = "Clipboard manager", group = "Applications" }
+		},
+
+		{
+			{ env.mod }, "l", focus_switch_byd("right"),
+			{ description = "Go to right client", group = "Client focus" }
+		},
+		{
+			{ env.mod }, "j", focus_switch_byd("left"),
+			{ description = "Go to left client", group = "Client focus" }
+		},
+		{
+			{ env.mod }, "i", focus_switch_byd("up"),
+			{ description = "Go to upper client", group = "Client focus" }
+		},
+		{
+			{ env.mod }, "k", focus_switch_byd("down"),
+			{ description = "Go to lower client", group = "Client focus" }
+		},
+		{
+			{ env.mod }, "u", awful.client.urgent.jumpto,
+			{ description = "Go to urgent client", group = "Client focus" }
+		},
+		{
+			{ env.mod }, "Tab", focus_to_previous,
+			{ description = "Go to previos client", group = "Client focus" }
+		},
+
+		{
+			{ env.mod }, "w", function() mainmenu:show() end,
+			{ description = "Show main menu", group = "Widgets" }
+		},
+		{
+			{ env.mod }, "r", function() apprunner:show() end,
+			{ description = "Application launcher", group = "Widgets" }
+		},
+		{
+			{ env.mod }, "p", function() redflat.float.prompt:run() end,
+			{ description = "Show the prompt box", group = "Widgets" }
+		},
+		{
+			{ env.mod, "Control" }, "i", function() redflat.widget.minitray:toggle() end,
+			{ description = "Show minitray", group = "Widgets" }
+		},
+
+		{
+			{ env.mod }, "Escape", awful.tag.history.restore,
+			{ description = "Go previos tag", group = "Tag navigation" }
+		},
+		{
+			{ env.mod }, "Right", awful.tag.viewnext,
+			{ description = "View next tag", group = "Tag navigation" }
+		},
+		{
+			{ env.mod }, "Left", awful.tag.viewprev,
+			{ description = "View previous tag", group = "Tag navigation" }
+		},
+
+		{
+			{ env.mod }, "a", nil, function() appswitcher:show({ filter = current }) end,
+			{ description = "Switch to next with current tag", group = "Application switcher" }
+		},
+		{
+			{ env.mod }, "q", nil, function() appswitcher:show({ filter = current, reverse = true }) end,
+			{ description = "Switch to previous with current tag", group = "Application switcher" }
+		},
+		{
+			{ env.mod, "Shift" }, "a", nil, function() appswitcher:show({ filter = allscr }) end,
+			{ description = "Switch to next through all tags", group = "Application switcher" }
+		},
+		{
+			{ env.mod, "Shift" }, "q", nil, function() appswitcher:show({ filter = allscr, reverse = true }) end,
+			{ description = "Switch to previous through all tags", group = "Application switcher" }
+		},
+
+		{
+			{ env.mod }, "t", function() redtitle.toggle(client.focus) end,
+			{ description = "Show/hide titlebar for focused client", group = "Titlebar" }
+		},
+		{
+			{ env.mod, "Control" }, "t", function() redtitle.switch(client.focus) end,
+			{ description = "Switch titlebar view for focused client", group = "Titlebar" }
+		},
+		{
+			{ env.mod, "Shift" }, "t", function() redtitle.toggle_all() end,
+			{ description = "Show/hide titlebar for all clients", group = "Titlebar" }
+		},
+		{
+			{ env.mod, "Control", "Shift" }, "t", function() redtitle.switch_all() end,
+			{ description = "Switch titlebar view for all clients", group = "Titlebar" }
+		},
+
+		{
+			{ env.mod }, "y", function() laybox:toggle_menu(mouse.screen.selected_tag) end,
+			{ description = "Show layout menu", group = "Layouts" }
+		},
+		{
+			{ env.mod, "Control"}, "Right", function() awful.layout.inc(1) end,
+			{ description = "Select next layout", group = "Layouts" }
+		},
+		{
+			{ env.mod, "Control" }, "Left", function() awful.layout.inc(-1) end,
+			{ description = "Select previous layout", group = "Layouts" }
+		},
+
+		{
+			{ env.mod, "Control" }, "n", restore_client,
+			{ description = "Restore minimized client", group = "Clients manipulation" }
+		},
+		{
+			{ env.mod }, "Down", minimize_all,
+			{ description = "Minimized all clients with current tag", group = "Clients manipulation" }
+		},
+		{
+			{ env.mod, "Control" }, "Down", minimize_all_except_focused,
+			{ description = "Minimized all clients except focused", group = "Clients manipulation" }
+		},
+		{
+			{ env.mod }, "Up", restore_all,
+			{ description = "Restore all clients with current tag", group = "Clients manipulation" }
+		},
+		{
+			{ env.mod, "Shift" }, "F4", kill_all,
+			{ description = "Kill all clients with current tag", group = "Clients manipulation" }
+		},
+	}
+
+	-- Client keys
+	--------------------------------------------------------------------------------
+	self.raw.client = {
+		{
+			{ env.mod }, "f", function(c) c.fullscreen = not c.fullscreen; c:raise() end,
+			{ description = "Toggle fullscreen", group = "Client keys" }
+		},
+		{
+			{ env.mod }, "F4", function(c) c:kill() end,
+			{ description = "Close", group = "Client keys" }
+		},
+		{
+			{ env.mod, "Control" }, "f", awful.client.floating.toggle,
+			{ description = "Toggle floating", group = "Client keys" }
+		},
+		{
+			{ env.mod, "Control" }, "o", function(c) c.ontop = not c.ontop end,
+			{ description = "Toggle keep on top", group = "Client keys" }
+		},
+		{
+			{ env.mod }, "n", function(c) c.minimized = true end,
+			{ description = "Minimize", group = "Client keys" }
+		},
+		{
+			{ env.mod }, "m", function(c) c.maximized = not c.maximized; c:raise() end,
+			{ description = "Maximize", group = "Client keys" }
+		}
+	}
+
+	self.keys.root = redflat.util.key.build(self.raw.root)
+	self.keys.client = redflat.util.key.build(self.raw.client)
+
+	-- Numkeys
+	--------------------------------------------------------------------------------
+
+	-- add real keys without description here
+	for i = 1, 9 do
+		self.keys.root = awful.util.table.join(
+			self.keys.root,
+			tag_numkey(i,    { env.mod },                     function(t) t:view_only()               end),
+			tag_numkey(i,    { env.mod, "Control" },          function(t) awful.tag.viewtoggle(t)     end),
+			client_numkey(i, { env.mod, "Shift" },            function(t) client.focus:move_to_tag(t) end),
+			client_numkey(i, { env.mod, "Control", "Shift" }, function(t) client.focus:toggle_tag(t)  end)
+		)
+	end
+
+	-- make fake keys with description special for key helper widget
+	local numkeys = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }
+
+	self.fake.numkeys = {
+		{
+			{ env.mod }, "1..9", nil,
+			{ description = "Switch to tag", group = "Numeric keys", keyset = numkeys }
+		},
+		{
+			{ env.mod, "Control" }, "1..9", nil,
+			{ description = "Toggle tag", group = "Numeric keys", keyset = numkeys }
+		},
+		{
+			{ env.mod, "Shift" }, "1..9", nil,
+			{ description = "Move focused client to tag", group = "Numeric keys", keyset = numkeys }
+		},
+		{
+			{ env.mod, "Control", "Shift" }, "1..9", nil,
+			{ description = "Toggle focused client on tag", group = "Numeric keys", keyset = numkeys }
+		},
+	}
+
+	-- Hotkeys helper setup
+	--------------------------------------------------------------------------------
+	redflat.float.hotkeys:set_pack("Main", awful.util.table.join(self.raw.root, self.raw.client, self.fake.numkeys), 2)
+
+	-- Mouse buttons
+	--------------------------------------------------------------------------------
+	self.mouse.client = awful.util.table.join(
+		awful.button({}, 1, function (c) client.focus = c; c:raise() end),
+		awful.button({}, 2, awful.mouse.client.move),
+		awful.button({ env.mod }, 3, awful.mouse.client.resize),
+		awful.button({}, 8, function(c) c:kill() end)
+	)
+
+	-- Set root hotkeys
+	--------------------------------------------------------------------------------
+	root.keys(self.keys.root)
+	root.buttons(self.mouse.root)
+end
+
+-- End
+-----------------------------------------------------------------------------------------------------------------------
+return hotkeys
