248896bddcc0da899bcaabde097871d853a245d7
worron <worrongm@gmail.com>
2017-03-22 21:13:03 +0300

    Set user map layout

--- colorless/layout-config.lua
+++ colorless/layout-config.lua
@@ -6,7 +6,6 @@
 local awful = require("awful")
 local redflat = require("redflat")
 
-
 -- Initialize tables and vars for module
 -----------------------------------------------------------------------------------------------------------------------
 local layouts = {}
@@ -17,6 +16,7 @@ local layouts = {}
 function layouts:init(args)
 	local args = args or {}
 
+	-- layouts list
 	local layset = {
 		awful.layout.suit.floating,
 		awful.layout.suit.tile,
@@ -27,14 +27,34 @@ function layouts:init(args)
 		awful.layout.suit.max,
 		awful.layout.suit.max.fullscreen,
 		redflat.layout.grid,
+		redflat.layout.map,
 	}
 
 	awful.layout.layouts = layset
 end
 
-
+-- connect alternatve moving handler to allow using custom handler per layout
+-- by now custom handler provided for 'redflat.layout.grid' only
+-- feel free to remove if you don't use this one
 client.disconnect_signal("request::geometry", awful.layout.move_handler)
 client.connect_signal("request::geometry", redflat.layout.common.mouse.move)
+
+
+-- connect additional signal for 'redflat.layout.map'
+-- this one removing client in smart way and correct tiling scheme
+-- feel free to remove if you want to restore plain queue behavior
+client.connect_signal("unmanage", redflat.layout.map.clean_client)
+
+client.connect_signal("property::minimized", function(c)
+	if c.minimized and redflat.layout.map.check_client(c) then redflat.layout.map.clean_client(c) end
+end)
+client.connect_signal("property::floating", function(c)
+	if c.floating and redflat.layout.map.check_client(c) then redflat.layout.map.clean_client(c) end
+end)
+
+client.connect_signal("untagged", function(c, t)
+	if redflat.layout.map.data[t] then redflat.layout.map.clean_client(c) end
+end)
 
 -- End
 -----------------------------------------------------------------------------------------------------------------------
