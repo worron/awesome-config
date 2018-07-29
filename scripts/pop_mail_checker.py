a8582db46ed0cac1411079556523931cc23c0b35
worron <worrongm@gmail.com>
2018-07-30 00:43:10 +0300

    Clean up mail check script

--- scripts/pop_mail_checker.py
+++ scripts/pop_mail_checker.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python
 
+# Primitive script which can be used with mail widget.
+# It's mostly demo assuming user can use something more advanced.
+
 import poplib
-from email import parser
 
 pop_conn = poplib.POP3_SSL('pop.gmail.com')
 pop_conn.user('username')
 pop_conn.pass_('password')
-#Get message num from server:
+
+# Get message num from server:
 numMsgs, totalSize = pop_conn.stat()
-#Print result:
-print numMsgs
-#Quit:
+
+# Print result:
+print(numMsgs)
+
+# Quit:
 pop_conn.quit()
