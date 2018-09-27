77858607501a3952db0752eede5d199fa81a94b2
worron <worrongm@gmail.com>
2018-09-27 19:47:20 +0300

    Work in progress

--- rc.lua
+++ rc.lua
@@ -1,5 +1,11 @@
+-- fix unpack compatibility between lua versions
 unpack = unpack or table.unpack
 
+-- disable autostart applications
+local timestamp = require("redflat.timestamp")
+timestamp.lock = true
+
+-- config selection
 -- local rc = "rc-red"
 local rc = "rc-blue"
 -- local rc = "rc-orange"
