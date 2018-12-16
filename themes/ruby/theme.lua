4cacdbc164e99582c06a9531d25bcd355146874b
worron <worrongm@gmail.com>
2018-12-16 03:11:42 +0300

    Ruby theme fixes

--- themes/ruby/theme.lua
+++ themes/ruby/theme.lua
@@ -37,19 +37,27 @@ theme:update()
 -- individual margins for palnel widgets
 ------------------------------------------------------------
 theme.widget.wrapper = {
-	layoutbox   = { 12, 10, 7, 7 },
+	layoutbox   = { 12, 10, 7, 6 },
 	textclock   = { 10, 10, 0, 0 },
-	volume      = { 10, 10, 7, 7 },
-	network     = { 6, 6, 8, 8 },
-	keyboard    = { 10, 10, 5, 5 },
+	volume      = { 10, 10, 6, 6 },
+	network     = { 6, 6, 7, 7 },
+	keyboard    = { 10, 10, 4, 3 },
 	taglist     = { 5, 5, 5, 5 },
-	mail        = { 10, 10, 5, 5 },
-	cpu         = { 10, 2, 8, 8 },
-	ram         = { 2, 2, 8, 8 },
-	battery     = { 2, 10, 8, 8 },
-	tray        = { 10, 12, 8, 8 },
+	mail        = { 10, 10, 4, 3 },
+	cpu         = { 10, 3, 7, 7 },
+	ram         = { 2, 2, 7, 7 },
+	battery     = { 3, 10, 7, 7 },
+	tray        = { 10, 12, 7, 7 },
 	--tasklist    = { 6, 0, 0, 0 }, -- centering tasklist widget
 }
+
+-- Dotcount
+------------------------------------------------------------
+theme.gauge.graph.dots.dot_gap_h = 5
+
+-- Dash
+------------------------------------------------------------
+theme.gauge.monitor.dash.width = 11
 
 
 -- Tag (base element of taglist)
