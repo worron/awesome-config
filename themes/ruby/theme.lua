5c59bb2f441f00134dfc7b84473a3b4d02c5a03f
worron <worrongm@gmail.com>
2018-12-16 03:26:49 +0300

    Ruby panel height reduce

--- themes/ruby/theme.lua
+++ themes/ruby/theme.lua
@@ -20,7 +20,7 @@ theme.path = awful.util.get_configuration_dir() .. "themes/ruby"
 
 -- Main config
 --------------------------------------------------------------------------------
-theme.panel_height = 40 -- panel height
+theme.panel_height = 36 -- panel height
 theme.wallpaper    = theme.path .. "/wallpaper/custom.png"
 
 -- Setup parent theme settings
@@ -37,23 +37,23 @@ theme:update()
 -- individual margins for palnel widgets
 ------------------------------------------------------------
 theme.widget.wrapper = {
-	layoutbox   = { 12, 10, 7, 6 },
+	layoutbox   = { 12, 10, 6, 5 },
 	textclock   = { 10, 10, 0, 0 },
-	volume      = { 10, 10, 6, 6 },
-	network     = { 6, 6, 7, 7 },
-	keyboard    = { 10, 10, 4, 3 },
-	taglist     = { 5, 5, 5, 5 },
-	mail        = { 10, 10, 4, 3 },
-	cpu         = { 10, 3, 7, 7 },
-	ram         = { 2, 2, 7, 7 },
-	battery     = { 3, 10, 7, 7 },
-	tray        = { 10, 12, 7, 7 },
+	volume      = { 10, 10, 5, 4 },
+	keyboard    = { 10, 10, 3, 3 },
+	mail        = { 10, 10, 3, 3 },
+	tray        = { 8, 8, 7, 7 },
+	cpu         = { 9, 3, 6, 6 },
+	ram         = { 2, 2, 6, 6 },
+	battery     = { 3, 9, 6, 6 },
+	network     = { 4, 4, 6, 5 },
+	taglist     = { 5, 5, 5, 4 },
 	--tasklist    = { 6, 0, 0, 0 }, -- centering tasklist widget
 }
 
 -- Dotcount
 ------------------------------------------------------------
-theme.gauge.graph.dots.dot_gap_h = 5
+--theme.gauge.graph.dots.dot_gap_h = 5
 
 -- Dash
 ------------------------------------------------------------
