e089add1a81a10bfb87a15c9a1df0a865d118262
worron <worrongm@gmail.com>
2019-04-25 19:56:38 +0300

    Add svg cleaner script

---
+++ scripts/clean-svg.py
@@ -0,0 +1,11 @@
+import os
+import subprocess
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+theme_dir = os.path.join(current_dir, '..', 'themes')
+
+for root, _, files in os.walk(theme_dir):
+	for name in files:
+		if name.endswith('.svg'):
+			image = os.path.join(root, name)
+			subprocess.call(['svgcleaner', '--indent=2', image, image])
